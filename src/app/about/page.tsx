49cb0a4489fed621d51b16763850852602590d6a
Bokobelin <alistair.richelle@gmail.com>
2025-03-18 16:52:14 +0100

    Remove spacing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,16 +10,13 @@ export default function About() {
                     Matthieu Richelle is a biblical scholar and epigrapher. He is professor of Old Testament exegesis at <Link href="https://www.uclouvain.be/en" className="text-blue-500 underline">UCLouvain (Université catholique de Louvain; Belgium)</Link>. A former student of the <Link href="https://www.ebaf.edu/en/" className="underline text-blue-500">Ecole biblique et archéologique française de Jérusalem</Link>, he holds a PhD in History from the <Link href="https://www.ephe.psl.eu/" className="underline text-blue-500">Ecole Pratique des Hautes Etudes</Link> and a Habilitation from the University of Strasbourg.
                 </div>
                 <br />
-                <br />
                 <div className="inline">
                     His research interests include Old Testament textual criticism, the exegesis of the Books of Kings, Semitic inscriptions of the ancient Levant, the relationship between the Bible and archaeology, and the history of ancient Israel. He is an associate editor of the <Link href="https://www.die-bibel.de/en/biblia-hebraica-quinta-bhq" className="text-blue-500 underline"><i>Biblia Hebraica Quinta</i></Link> and the principal editor of 1 Kings (in progress) in the series <Link href="https://www.sbl-site.org/HBCE/HBCE_About.html" className="text-blue-500 underline">Hebrew Bible: A Critical Edition (HBCE)</Link>.
                 </div>
                 <br />
-                <br />
                 <div className="inline">
                     He is currently co-editing (with Jeremy Hutton) the <i>Oxford Handbook of Northwest Semitic Epigraphy</i> and co-editing (with Régis Burnet) a new edition of the <i>Bible de Jérusalem</i>.
                     <br />  
-                    <br /> 
                     <Link href="/List of publications 18 March 2025.pdf" className="text-blue-500 underline inline">List of Publications</Link>
                 </div>
             </main>
