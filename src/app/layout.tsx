7f8d372adf70ce4dd20eb21f9d80747bba82e36e
Bokobelin <mrgeek915@gmail.com>
2026-05-15 23:28:13 +0200

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import Cookies from "@/components/cookies";
 
@@ -8,6 +9,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -27,28 +29,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//matomo.bokobelin.top:7075/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
+        <Script id="matomo" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
 
+            (function() {
+              var u="//matomo.bokobelin.top:7075/";
+              _paq.push(['setTrackerUrl', u + 'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+
+              var d=document,
+                  g=d.createElement('script'),
+                  s=d.getElementsByTagName('script')[0];
+
+              g.async=true;
+              g.src=u+'matomo.js';
+              s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         {children}
-        <Cookies /> 
+        <Cookies />
       </body>
     </html>
   );
