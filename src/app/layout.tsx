da0dbd72439ac21fd84ab204098361a7dcfc9d89
Bokobelin <mrgeek915@gmail.com>
2026-05-16 11:50:51 +0200

    Add cookie consent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,30 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script id="matomo" strategy="afterInteractive">
-          {`
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-
-            (function() {
-              var u="//matomo.bokobelin.top/";
-              _paq.push(['setTrackerUrl', u + 'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-
-              var d=document,
-                  g=d.createElement('script'),
-                  s=d.getElementsByTagName('script')[0];
-
-              g.async=true;
-              g.src=u+'matomo.js';
-              s.parentNode.insertBefore(g,s);
-            })();
-          `}
-        </Script>
-      </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
